d1061f2bc229645ec2cb9e238253f8432348f3b2
Jeff Alexander <12126499+alexander-jeff@users.noreply.github.com>
2022-07-16 20:54:07 -0700

    Use bootstrap

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,4 +1,17 @@
-<nav>
-	<a href="/">Home</a>
-	<a href="/about">About</a>
-</nav>
+<script>
+	import { Nav, NavItem, NavLink } from 'sveltestrap';
+</script>
+
+<div class="nav-bar">
+	<Nav>
+		<NavLink href="/">Home</NavLink>
+		<NavLink href="/about">About</NavLink>
+	</Nav>
+</div>
+
+<style>
+	.nav-bar {
+		font-family: 'Nunito', sans-serif;
+		color: black;
+	}
+</style>
